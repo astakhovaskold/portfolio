431d4da0f391c635ebe38dc5c4210546f2684fdf
Askold Astakhov <astakhovaskold@gmail.com>
2025-01-15 13:57:03 +0300

    update robots and sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,21 +7,21 @@ export default async function sitemap() {
 
     let blogs = locales.flatMap(locale =>
         getPosts(['src', 'app', '[locale]', 'blog', 'posts', locale]).map(post => ({
-            url: `${baseURL}/${locale}/blog/${post.slug}`,
+            url: `https://${baseURL}/${locale}/blog/${post.slug}`,
             lastModified: post.metadata.publishedAt,
         })),
     );
 
     let works = locales.flatMap(locale =>
         getPosts(['src', 'app', '[locale]', 'work', 'projects', locale]).map(post => ({
-            url: `${baseURL}/${locale}/work/${post.slug}`,
+            url: `https://${baseURL}/${locale}/work/${post.slug}`,
             lastModified: post.metadata.publishedAt,
         })),
     );
 
     let routes = locales.flatMap(locale =>
         ['', '/blog', '/work'].map(route => ({
-            url: `${baseURL}/${locale}${route}`,
+            url: `https://${baseURL}/${locale}${route}`,
             lastModified: new Date().toISOString().split('T')[0],
         })),
     );
